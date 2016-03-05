ba5bb541f1307451af812f1f665da299ddb0e235
drdhaval2785 <drdhaval2785@gmail.com>
2016-03-05 13:52:18 +0530

    corrections.sh back to base

--- scripts/corrections.sh
+++ scripts/corrections.sh
@@ -1,7 +1,6 @@
-#LIST1=(balamanorama kashika laghu nyasa samhita tattvabodhini)
-#LIST2=(pada-1.1 pada-1.2 pada-1.3 pada-1.4 pada-2.1 pada-2.2 pada-2.3 pada-2.4 pada-3.1 pada-3.2 pada-3.3 pada-3.4 pada-4.1 pada-4.2 pada-4.3 pada-4.4 pada-5.1 pada-5.2 pada-5.3 pada-5.4 pada-6.1 pada-6.2 pada-6.3 pada-6.4 pada-7.1 pada-7.2 pada-7.3 pada-7.4 pada-8.1 pada-8.2 pada-8.3 pada-8.4)
-LIST1=(tattvabodhini)
-LIST2=(pada-6.2 pada-6.3 pada-6.4 pada-7.1 pada-7.2 pada-7.3 pada-7.4 pada-8.1 pada-8.2 pada-8.3 pada-8.4)
+LIST1=(balamanorama kashika laghu nyasa samhita tattvabodhini)
+LIST2=(pada-1.1 pada-1.2 pada-1.3 pada-1.4 pada-2.1 pada-2.2 pada-2.3 pada-2.4 pada-3.1 pada-3.2 pada-3.3 pada-3.4 pada-4.1 pada-4.2 pada-4.3 pada-4.4 pada-5.1 pada-5.2 pada-5.3 pada-5.4 pada-6.1 pada-6.2 pada-6.3 pada-6.4 pada-7.1 pada-7.2 pada-7.3 pada-7.4 pada-8.1 pada-8.2 pada-8.3 pada-8.4)
+"""
 cd ..
 for BOOK in "${LIST1[@]}"
 do
@@ -22,6 +21,7 @@ do
 	done
 	cd ..
 done
+"""
 
 """
 # rephalist scraping
